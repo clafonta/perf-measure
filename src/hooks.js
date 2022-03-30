1852ef21d269de1ba56534f92fc282bfec4cd929
Chad Lafontaine <chad.lafontaine@gmail.com>
2022-03-30 11:04:49 -0700

    added minification of HTML

--- src/hooks.js
+++ src/hooks.js
@@ -1,11 +1,17 @@
 import cookie from 'cookie';
 import { v4 as uuid } from '@lukeed/uuid';
+import { minify } from 'html-minifier'; //Imports the module
+
 
 export const handle = async ({ event, resolve }) => {
 	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
 	event.locals.userid = cookies.userid || uuid();
 
 	const response = await resolve(event);
+
+	if (response.headers['content-type'] === 'text/html') {
+		response.body = minify(response.body, minification_options); //Minifies the response.body
+	}
 
 	if (!cookies.userid) {
 		// if this is the first time the user has visited this app,
@@ -21,3 +27,25 @@ export const handle = async ({ event, resolve }) => {
 
 	return response;
 };
+
+const minification_options = {
+	collapseBooleanAttributes: true,
+	collapseWhitespace: true,
+	conservativeCollapse: true,
+	decodeEntities: true,
+	html5: true,
+	ignoreCustomComments: [/^#/],
+	minifyCSS: true,
+	minifyJS: true,
+	removeAttributeQuotes: true,
+	removeComments: true,
+	removeOptionalTags: true,
+	removeRedundantAttributes: true,
+	removeScriptTypeAttributes: true,
+	removeStyleLinkTypeAttributes: true,
+	sortAttributes: true,
+	sortClassName: true,
+	removeEmptyElements: true
+};
+
+
